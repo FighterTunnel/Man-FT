73522b192900f07e8a301985424b7b49c5fd59f2
yha_bot <118927603+FighterTunnel@users.noreply.github.com>
2023-05-10 04:23:25 +0700

    FighterTunnel

--- ubot.sh
+++ ubot.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# FighterTunnel
+# Copyright (C) 2023 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
 function string() {
         clear
         read -e -p "Create String Sessions? [y/n] " -i "y" str
