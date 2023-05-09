6e621be6298d188aaacf01cff2640f1475d9fd8b
yha_bot <118927603+FighterTunnel@users.noreply.github.com>
2023-05-10 05:57:23 +0700

    FighterTunnel

--- ft/resources/session/str.py
+++ ft/resources/session/str.py
@@ -15,8 +15,6 @@ Click on API Development Tools
 Create a new application, by entering the required details
 Check your Telegram saved messages section to copy the STRING_SESSION"""
 )
-API_KEY = int(input("Enter API_KEY here: "))
-API_HASH = input("Enter API_HASH here: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     print("Check Telegram Save Message Mu Untuk Copy STRING_SESSION ")
